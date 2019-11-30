c985e7fe680da7df5b8fdbbf0e97926d1227131e
NZsolt98 <nagyzsolt873@gmail.com>
2019-11-30 18:17:26 +0100

    Deleted DuplikaltAuto.java and KocsiNemTalalhato.java because it's not required anymore by the project

--- dao-json/src/main/java/dao/DuplikaltAuto.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public class DuplikaltAuto extends Exception {
-
-    public DuplikaltAuto(String s) {
-        super(s);
-    }
-}
--- dao-json/src/main/java/dao/KocsiNemTalalhato.java
+++
@@ -1,4 +0,0 @@
-package dao;
-
-public class KocsiNemTalalhato extends Throwable {
-}
