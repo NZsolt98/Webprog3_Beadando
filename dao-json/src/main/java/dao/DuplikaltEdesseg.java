45f685a6a4c0df94a21c9af10582f3544ea05168
NZsolt98 <nagyzsolt873@gmail.com>
2019-11-30 18:18:51 +0100

    Created DuplikaltEdesseg.java and EdessegNemTalalhato.java

---
+++ dao-json/src/main/java/dao/DuplikaltEdesseg.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class DuplikaltEdesseg extends Exception {
+
+    public DuplikaltEdesseg(String s) {
+        super(s);
+    }
+}
---
+++ dao-json/src/main/java/dao/EdessegNemTalalhato.java
@@ -0,0 +1,4 @@
+package dao;
+
+public class EdessegNemTalalhato extends Throwable {
+}
